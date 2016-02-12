b311c70443b541902808dd2d860e9a45036aeaf8
Chris Marshall <chris@chrismar035.com>
2016-02-12 16:43:36 -0500

    Deploy by sync and invalidate cloudfront cache

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 hugo
 
-aws s3 cp public/ s3://codegoalie.com/ \
-  --recursive \
+aws s3 sync public/ s3://codegoalie.com/ \
   --profile blog \
+  --delete \
   --acl public-read 
+
+aws cloudfront create-invalidation --distribution-id E2VC2R7NUNA9O8 \
+  --path / \
+  --profile blog
